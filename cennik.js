3a8ec56bfd70dcf7d29cf7343dd5198521a519f6
roxol11 <62464106+roxol11@users.noreply.github.com>
2020-05-23 16:38:03 +0200

    Initial commit

---
+++ cennik.js
@@ -0,0 +1,35 @@
+function Click() {
+  var x = document.getElementById("car_cash_list");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Click2() {
+  var x = document.getElementById("car_cash_list2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Click3() {
+  var x = document.getElementById("car_cash_list3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Click4() {
+  var x = document.getElementById("car_cash_list4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
